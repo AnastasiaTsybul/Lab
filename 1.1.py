d34cdc77249e15a554035806ae6dcc64f5a9dccb
AnastasiaTsybul <nastya13mif@gmail.com>
2025-10-06 03:07:28 +0300

    1.1.py

--- 1.1.py
+++ 1.1.py
@@ -1,35 +1,35 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
-# Пример: загрузим DataFrame (замени путь к CSV при необходимости)
+
 df = pd.read_csv("coffe.csv")
 
-# Проверим пропуски
+
 nul_matrix = df.isnull()
 print(nul_matrix.sum())
 
-# Заполнение пропусков
-cabin_mode = df['Cabin'].mode()[0]
-room_median = df['RoomService'].median()
-age_mean = df['Age'].mean()
 
-df['Cabin'].fillna(cabin_mode, inplace=True)
-df['RoomService'].fillna(room_median, inplace=True)
-df['Age'].fillna(age_mean, inplace=True)
+money_mode = df['money'].mode()[0]
+room_median = df['hour_of_day'].median()
+age_mean = df['Weekdaysort'].mean()
 
-# Проверка после заполнения
+df['money'].fillna(money_mode, inplace=True)
+df['hour_of_day'].fillna(room_median, inplace=True)
+df['Weekdaysort'].fillna(age_mean, inplace=True)
+
+
 nul_matrix = df.isnull()
-print(nul_matrix.sum())
+print (nul_matrix.sum())
 
-# Масштабирование возраста
+
 scaler = MinMaxScaler()
-scaler.fit(df[['Age']])
-df['Age'] = scaler.transform(df[['Age']])
+scaler.fit(df[['Weekdaysort']])
+df['Weekdaysort'] = scaler.transform(df[['Weekdaysort']])
 
-# Преобразование категориальных признаков
+
 df = pd.get_dummies(df, drop_first=True)
 
-# Сохраним результат
-df.to_csv('train.csv', index=False)
+
+df.to_csv('coffe.csv', index=False)
 
 print(df.head())
